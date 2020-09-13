a9b8c4ee0239206833bb043bc3e1e6e5d383f55b
Hector G <hecgarcia.n@gmail.com>
2020-09-13 11:05:33 -0500

    Animaciones Carga
    - se le agrego animación a la carga del programa

--- web_folder/js/funciones.js
+++ web_folder/js/funciones.js
@@ -1,21 +1,27 @@
-'use stric'
 
+window.onload = function () {
+    setTimeout(carga, 4000);
+}
 
+function carga(){
+    $('#preloader').fadeOut();
+    $('#menu-principal').removeClass('hide');
+}
 
-async function mostrar_ruta_metadatos(){
-    let ruta = await eel.ruta_metadatos()();   
+async function mostrar_ruta_metadatos() {
+    let ruta = await eel.ruta_metadatos()();
     document.getElementsByName("ruta-reci")[0].value = ruta;
     console.log(ruta);
 }
 
-async function mostrar_en_tabla(){
+async function mostrar_en_tabla() {
 
     let directorio = document.getElementsByName("ruta-reci")[0].value;
     let rutas = await eel.mostrar_rutas_recibos(directorio)();
     const rutas_num = Object.getOwnPropertyNames(rutas)
 
     debugger;
-    for(let i=1; i<rutas_num.length; i++){
+    for (let i = 1; i < rutas_num.length; i++) {
 
         let tr = document.createElement('tr');
         let rutas_datos_num = Object.getOwnPropertyDescriptor
@@ -27,9 +33,9 @@ async function mostrar_en_tabla(){
         }
 
 
-        
 
-    
-    
-}
+
+
+
+    }
 }
