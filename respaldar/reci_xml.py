0f33f82fa07b524898d2fea4f34d250135119433
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2020-09-24 10:55:23 -0500

    Copiado Recibos - BACKUP
    - se copian archivos de recibos de nomina al dsico de backup
    - se selecciona desde interfaz el archivo a copiar
    - se validan los Inputs que estén con datos desde la interfaz

--- respaldar/reci_xml.py
+++ respaldar/reci_xml.py
@@ -1,6 +1,7 @@
 from os.path import splitext
 
 from modulos.rutas import Rutas, unir_cadenas
+from respaldar.modulos.archivo import Archivo
 
 class ArchivosOrig:     
 	
@@ -64,3 +65,30 @@ class ArchivosOrig:
 	def archivos_pdf_jub(self, datos):
 		if datos[self.ruta_num+1].upper() == 'RECIBOS':
 			return datos
+
+
+
+class ArchivoRecibo(Archivo):
+	"""Clase que forma los datos
+	y llama a los metodos correspondientes
+	para el backup limpio de los recibos de nomina 
+	"""
+	def __init__(self, orig_carpeta, ruta_orig, carpeta_dest, nombre, copiar):
+		self.carpeta_orig = orig_carpeta.replace('/', '\\')	
+		self.carpeta_dest = carpeta_dest.replace('/', '\\')	
+		
+		self.origen =  ruta_orig
+		self.destino = self.formar_ruta_destino()	
+		
+		self.nombre = nombre
+		self.copiar = copiar
+		Archivo.__init__(self, self.origen, self.destino, self.nombre,  self.copiar)
+		
+	def formar_ruta_destino(self):
+		destino = self.origen.replace(self.carpeta_orig, self.carpeta_dest)
+
+		
+		
+		return destino
+
+	
--- web_folder/js/respaldar.js
+++ web_folder/js/respaldar.js
@@ -1,42 +1,83 @@
-async function mostrarRutas(){
+async function mostrarRutas() {
     /* 
         let datos = document.getElementById("tbl-datos");
         datos.removeChild(tr) */
-        
-        let tabla =document.getElementById("tbl");  
-        let carga = document.createElement("div");
-        let ruta = document.getElementsByName("ruta")[0].value;
-        let anno = document.getElementsByName("anno")[0].value;
-        let periodo = document.getElementsByName("periodo")[0].value;
-        carga.setAttribute("class", "loading");    
-        tabla.appendChild(carga);    
-        
-        let recibos = await eel.rutas_recibos_orig(ruta, anno, periodo)();
-        const recibosNum = Object.getOwnPropertyNames(recibos);
-            
-        for(let i=0; i<recibosNum.length; i++){
-            let lista = document.getElementById("tbl-datos");
-            let tr = document.createElement("tr");      
-            let checkBox = document.createElement("input");
-            checkBox.setAttribute("type", "checkbox");
-            checkBox.setAttribute("class", "c-box");
-            
-    
-            let ultimo = recibos[recibosNum[i]]["length"];
-            let columnaPeriodo = document.createElement("td");
-            columnaPeriodo.innerHTML = recibos[recibosNum[i]][3];
-            let columnaNomina = document.createElement("td");
-            columnaNomina.innerHTML = recibos[recibosNum[i]][4];    
-            let columnaArchivo = document.createElement("td");     
-            columnaArchivo.innerHTML = recibos[recibosNum[i]][ultimo-1];   
-                
-            
-            lista.appendChild(tr);               
-            tr.appendChild(columnaPeriodo);
-            columnaPeriodo.appendChild(checkBox); 
-            tr.appendChild(columnaNomina);
-            tr.appendChild(columnaArchivo);
-        
-        }
-        tabla.removeChild(carga);
+
+    let tabla = document.getElementById("tbl");
+    let carga = document.createElement("div");
+    let ruta = document.getElementsByName("ruta")[0].value;
+    let anno = document.getElementsByName("anno")[0].value;
+    let periodo = document.getElementsByName("periodo")[0].value;
+    carga.setAttribute("class", "loading");
+    tabla.appendChild(carga);
+
+    let recibos = await eel.rutas_recibos_orig(ruta, anno, periodo)();
+    const recibosNum = Object.getOwnPropertyNames(recibos);
+
+    for (let i = 0; i < recibosNum.length; i++) {
+        let lista = document.getElementById("tbl-datos");
+        let tr = document.createElement("tr");
+        let checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("class", "c-box");
+
+
+        let ultimo = recibos[recibosNum[i]]["length"];
+
+        let columnaPeriodo = document.createElement("td");
+        columnaPeriodo.setAttribute("class", "cl-per")
+        columnaPeriodo.innerHTML = recibos[recibosNum[i]][3];
+        let columnaNomina = document.createElement("td");
+        columnaNomina.innerHTML = recibos[recibosNum[i]][4];
+        let columnaArchivo = document.createElement("td");
+        columnaArchivo.setAttribute("class", "ruta-archivo");
+        columnaArchivo.innerHTML = recibos[recibosNum[i]][ultimo - 1];
+
+
+        lista.appendChild(tr);
+        tr.appendChild(columnaPeriodo);
+        columnaPeriodo.appendChild(checkBox);
+        tr.appendChild(columnaNomina);
+        tr.appendChild(columnaArchivo);
+
     }
+    tabla.removeChild(carga);
+}
+
+
+function iniciarCopiado() {
+    let carp_orig = document.getElementsByName("ruta")[0].value;
+    let carp_dest = document.getElementsByName("ruta_destino")[0].value;
+    let archivos = [];
+    let filaPeriodo = document.getElementsByClassName("cl-per");
+
+    if (carp_dest != '' & carp_orig != '' &
+        filaPeriodo.length > 0){
+        for (let i = 0; i < filaPeriodo.length; i++) {
+            checkBox = filaPeriodo[i].getElementsByClassName("c-box");
+
+            if (checkBox[0].checked == true) {
+                let filaRuta = document.getElementsByClassName("ruta-archivo")[i].innerText;
+                archivos.push(filaRuta);
+            }
+            
+        }
+
+    }
+    else {
+        alert('Se debe seleccionar Directorios de Destino Y/O Origen y buscar Archivos Primero');
+    }
+
+    if(archivos.length > 0){
+        deshabilitar('principal');
+        loader_tarea();
+        eel.copiado_recibos(carp_orig, carp_dest, archivos);
+    }
+    
+    else{
+        alert('Seleccione almenos una fila');
+    }
+
+    habilitar('principal');
+    
+}
